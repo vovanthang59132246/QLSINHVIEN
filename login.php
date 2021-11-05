7ea17e7e3e281d95aba18abf95791deda541403c
vovanthang59132246 <63826792+vovanthang59132246@users.noreply.github.com>
2021-11-05 14:45:48 +0700

    Update login.php

--- login.php
+++ login.php
@@ -9,13 +9,12 @@ ob_start();//tắt lỗi header
     <title>Đăng nhập - QLSV</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
      <link rel="preconnect" href="https://fonts.gstatic.com">
      <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 
-    <link rel="stylesheet" type="text/css" href="css/qlsv.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
 
 </head>
 
@@ -39,6 +38,9 @@ ob_start();//tắt lỗi header
         <div class="signup_link">
           không phải là thành viên? <a href="register.php">Đăng ký</a>
         </div>
+        <div class="signup_link">
+          <a>create by Vo Van Thang</a>
+</div>
       </form>
     </div>
 
